4fdc873b53ec69b80d59c4303f1fb1d827b21be7
Kaan Kara <kaan.kara@yahoo.com>
2023-10-13 00:55:12 +0300

    PHP button
    Whatsapp button in PHP language

---
+++ wp-button.php
@@ -0,0 +1,13 @@
+‹script type="text/javascript"›
+    (function ()) {
+        var options = {
+            whatsapp: "+905*********",
+            call_to_action: "Nasıl yardımcı olabilirim?",
+            position: "left",
+        };
+        var photo = document.location.protocol, host = "getbutton.io", url = photo + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript' ; s.async = true; s.src = url1 + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetsendButton.init(host, proto, options); };
+        var x = document.getElementByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script>
