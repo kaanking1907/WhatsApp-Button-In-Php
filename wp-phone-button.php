307c31800af5afab5530196072d9dacafc513c48
Kaan Kara <kaan.kara@yahoo.com>
2023-10-14 12:41:45 +0300

    UPDATE

--- wp-phone-button.php
+++ wp-phone-button.php
@@ -1,18 +1,5 @@
-<script type="text/javascript">
-    (function () {
-        var options = {
-            whatsapp: "+905*********",
-            call_to_action: "Nasıl yardımcı olabilirim?",
-            position: "left",
-        };
-        var photo = document.location.protocol, host = "getbutton.io", url = photo + "//static." + host;
-        var s = document.createElement('script'); s.type = 'text/javascript' ; s.async = true; s.src = url1 + '/widget-send-button/js/init.js';
-        s.onload = function () { WhWidgetsendButton.init(host, proto, options); };
-        var x = document.getElementByTagName('script')[0]; x.parentNode.insertBefore(s, x);
-    })();
-    function btnClick() {
-        const linkElement = document.createElement("a")
-        linkElement.href = 'tel:+123456789'
-        linkElement.click()
-    }
-</script>
+<?php
+$telefonNumarasi = "TelefonNumarası"; 
+?>
+<a href="https://wa.me/<?php echo $telefonNumarasi; ?>" target="_blank">WhatsApp ile Konuş</a>
+<a href="tel:+<?php echo $telefonNumarasi; ?>">Ara</a>
