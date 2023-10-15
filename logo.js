977f4b9e2f98cbe5fcec72ba3a3f4cdbd2d12f60
Kaan Kara <kaan.kara@yahoo.com>
2023-10-15 11:50:18 +0300

    Version 1.3

--- logo.js
+++
@@ -1,9 +0,0 @@
-function ara() {
-    var telefonNumarasi = "TelefonNumarası"; 
-    window.location.href = "tel:" + telefonNumarasi;
-}
-
-function whatsappSohbetiBaslat() {
-    var telefonNumarasi = "TelefonNumarası"; 
-    window.open("https://wa.me/" + telefonNumarasi, "_blank");
-}
--- wp-phone-button.php
+++
@@ -1,5 +0,0 @@
-<?php
-$telefonNumarasi = "TelefonNumarası"; 
-?>
-<a href="https://wa.me/<?php echo $telefonNumarasi; ?>" target="_blank">WhatsApp ile Konuş</a>
-<a href="tel:+<?php echo $telefonNumarasi; ?>">Ara</a>
